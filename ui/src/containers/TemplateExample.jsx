0db94ea6b85a95b05848239dba9d0570d271baf0
ChamodSilva <chamodsilva@hotmail.com>
2025-04-12 17:21:19 +0930

    Lab 6 initial state

---
+++ ui/src/containers/TemplateExample.jsx
@@ -0,0 +1,16 @@
+import {Box} from '@mui/material';
+
+function TemplateExample()
+{
+    //Variables
+
+    //Functions
+
+    //Return
+    return(
+        <Box>
+            Template Example
+        </Box>);
+}
+
+export default TemplateExample;
